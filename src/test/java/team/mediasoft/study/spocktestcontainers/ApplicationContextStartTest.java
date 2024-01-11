09f190b3dd7387fa5766ab2ba3713db20aaa80b0
Pavel A Andryushenko <p.andryushenko@mediasoft.team>
2024-01-11 15:44:52 +0400

    Добавлены liquibase-скрипты создания объектов БД

--- src/test/java/team/mediasoft/study/spocktestcontainers/ApplicationContextStartTest.java
+++ src/test/java/team/mediasoft/study/spocktestcontainers/ApplicationContextStartTest.java
@@ -3,13 +3,22 @@ package team.mediasoft.study.spocktestcontainers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class}
+)
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class ApplicationContextStartTest {
 
     @Autowired
